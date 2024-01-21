41d62019c0afd7d634157636409873307b432298
Josh Colclough <colcloughj2@cardiff.ac.uk>
2024-01-21 17:29:00 +0000

    Implement iterative string reverse

--- recursive_reverse/main.cpp
+++ recursive_reverse/main.cpp
@@ -3,10 +3,18 @@
 #include <utility>
 
 void reverse(std::string::iterator begin, std::string::iterator end) {
-  if (--end - begin <= 0) return;
+  --end;
+  if (end - begin <= 0) return;
 
   std::swap(*begin, *end);
   return reverse(++begin, end);
+}
+
+void reverse_iterative(std::string::iterator begin, std::string::iterator end) {
+  for (auto it = begin; end - it > 0; ++it) {
+    --end;
+    std::swap(*it, *end);
+  }
 }
 
 
@@ -14,8 +22,9 @@ int main(int argc, char* argv[]) {
   std::string input;
   std::getline(std::cin, input);
 
-  std::cout << input << std::endl;
   reverse(input.begin(), input.end());
+  std::cout << input << std::endl;
+  reverse_iterative(input.begin(), input.end());
   std::cout << input << std::endl;
 
   return 0;
